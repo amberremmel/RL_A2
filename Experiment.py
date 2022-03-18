38704c8bfd32f1b4c1a9b673c4e67d48f39fc4e1
amberremmel <54839039+amberremmel@users.noreply.github.com>
2022-03-18 13:38:07 +0100

    Add files via upload

--- Experiment.py
+++ Experiment.py
@@ -37,97 +37,84 @@ def average_over_repetitions(n_repetitions, n_episodes=250,
 def experiment():
     ####### Settings
     # Experiment    
-    n_repetitions = 2
-    smoothing_window = 21#1001
+    n_repetitions = 2 #20
+    smoothing_window = 21 #1001
 
-    n_episodes = 500
+    n_episodes = 30 #500
     gamma = 0.9
-    learning_rate = 0.005
+    learning_rate = 0.05
     
     # Hidden layers
     n_nodes = [64, 32]
 
     # Exploration
     epsilon_max = 0.8
-    epsilon_min = 0.05
+    epsilon_min = 0.005
     epsilon_decay = 0.995
 
     # Experience replay
-    ER_buffer = True
+    ER_buffer = False
     ER_size = 200
     
     # After how much episodes the target network will be updated
     # When value of 1 is chosen > same as if no target network is used
     # Because the target network will then be updated at every episode
-    n_update_TN = 25
+    n_update_TN = 1 #25
 
     # Plotting parameters
     render = False
     
     ####### Experiments
-    
-    # #### Assignment 2: Effect of exploration
-    # backup = 'q'
-    # Plot = LearningCurvePlot(title = 'Q-learning: effect of $\epsilon$-greedy versus softmax exploration')    
-    # policy = 'egreedy'
-    # epsilons = [0.01,0.05,0.2]
-    # for epsilon in epsilons:        
-        # learning_curve = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                              # gamma, policy, epsilon, temp, smoothing_window, plot, n)
-        # Plot.add_curve(learning_curve,label=r'$\epsilon$-greedy, $\epsilon $ = {}'.format(epsilon))    
-    # policy = 'softmax'
-    # temps = [0.01,0.1,1.0]
-    # for temp in temps:
-        # learning_curve = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                              # gamma, policy, epsilon, temp, smoothing_window, plot, n)
-        # Plot.add_curve(learning_curve,label=r'softmax, $ \tau $ = {}'.format(temp))
-    # Plot.add_hline(optimal_average_reward_per_timestep, label="DP optimum")
-    # Plot.save('exploration.png')
-    # policy = 'egreedy'
-    # epsilon = 0.05 # set epsilon back to original value 
-    # temp = 1.0
-    
-    
-    # ###### Assignment 3: Q-learning versus SARSA
-    # backups = ['q','sarsa']
-    # learning_rates = [0.05,0.2,0.4]
-    # Plot = LearningCurvePlot(title = 'Q-learning versus SARSA')    
-    # for backup in backups:
-        # for learning_rate in learning_rates:
-            # learning_curve = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                                  # gamma, policy, epsilon, temp, smoothing_window, plot, n)
-            # Plot.add_curve(learning_curve,label=r'{}, $\alpha$ = {} '.format(backup_labels[backup],learning_rate))
-    # Plot.add_hline(optimal_average_reward_per_timestep, label="DP optimum")
-    # Plot.save('on_off_policy.png')
-    # # Set back to original values
-    # learning_rate = 0.25
-    # backup = 'q'
 
-
-    # # ##### Assignment 4: Back-up depth
-    # backup = 'nstep'
-    # ns = [1,3,5,10,20,100]
-    # Plot = LearningCurvePlot(title = 'Effect of target depth')    
-    # for n in ns:
-        # learning_curve = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                              # gamma, policy, epsilon, temp, smoothing_window, plot, n)
-        # Plot.add_curve(learning_curve,label=r'{}-step Q-learning'.format(n))
-    # backup = 'mc'
-    # learning_curve = average_over_repetitions(backup, n_repetitions, n_timesteps, max_episode_length, learning_rate, 
-                                          # gamma, policy, epsilon, temp, smoothing_window, plot, n)
-    # Plot.add_curve(learning_curve,label='Monte Carlo')        
-    # Plot.add_hline(optimal_average_reward_per_timestep, label="DP optimum")
-    # Plot.save('depth.png')
-
+    # Varying the learning_rates
     Plot = LearningCurvePlot(title = 'Deep Q-network') 
-    learning_rates = [0.1,0.05,0.01]   
+    learning_rates = [0.1,0.05,0.01,0.005, 0.001]
     for learning_rate in learning_rates:
         learning_curve = average_over_repetitions(n_repetitions, n_episodes,
                    learning_rate, gamma, n_nodes,epsilon_max, epsilon_min, 
                    epsilon_decay, ER_buffer, ER_size, n_update_TN, render, 
                    smoothing_window)
         Plot.add_curve(learning_curve,label=r'$\alpha$ = {} '.format(learning_rate))        
-    Plot.save('dqn_result.png')
+    Plot.save('dqn_result_alpha={}.png'.format(learning_rates))
+    
+    learning_rate = 0.005
+
+    # Varying the discount parameter
+    Plot = LearningCurvePlot(title = 'Deep Q-network') 
+    gammas = [0.9,0.7,0.5,1]   
+    for gamma in gammas:
+        learning_curve = average_over_repetitions(n_repetitions, n_episodes,
+                   learning_rate, gamma, n_nodes,epsilon_max, epsilon_min, 
+                   epsilon_decay, ER_buffer, ER_size, n_update_TN, render, 
+                   smoothing_window)
+        Plot.add_curve(learning_curve,label=r'$\gamma$ = {} '.format(gamma))        
+    Plot.save('dqn_result_gamma={}.png'.format(gammas))
+
+    gamma = 0.9
+
+    # Varying the epsilon decay
+    Plot = LearningCurvePlot(title = 'Deep Q-network') 
+    epsilon_decays = [0.99,0.995,0.98]   
+    for epsilon_decay in epsilon_decays:
+        learning_curve = average_over_repetitions(n_repetitions, n_episodes,
+                   learning_rate, gamma, n_nodes,epsilon_max, epsilon_min, 
+                   epsilon_decay, ER_buffer, ER_size, n_update_TN, render, 
+                   smoothing_window)
+        Plot.add_curve(learning_curve,label=r'$\epsilon$-decay = {} '.format(epsilon_decay))        
+    Plot.save('dqn_result_epsilon_decay={}.png'.format(epsilon_decays))
+
+    epsilon_decay = 0.995
+
+    # Varying the number of layers
+    Plot = LearningCurvePlot(title = 'Deep Q-network') 
+    n_nodess = [[32], [32, 32], [32, 64, 32], [32, 64, 32, 16]]   
+    for n_nodes in n_nodess:
+        learning_curve = average_over_repetitions(n_repetitions, n_episodes,
+                   learning_rate, gamma, n_nodes,epsilon_max, epsilon_min, 
+                   epsilon_decay, ER_buffer, ER_size, n_update_TN, render, 
+                   smoothing_window)
+        Plot.add_curve(learning_curve,label='# layers = {} '.format(n_nodes))        
+    Plot.save('dqn_result_n_layers={}.png'.format([len(n_nodes) for n_nodes in n_nodess]))    
     
 if __name__ == '__main__':
     experiment()
