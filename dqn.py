1494eb1ba222145c010202ede40686fe722ae1a3
amberremmel <54839039+amberremmel@users.noreply.github.com>
2022-04-01 15:37:24 +0200

    Add files via upload

--- dqn.py
+++ dqn.py
@@ -14,9 +14,11 @@ class DQN_agent:
     def __init__(self, n_actions=2, n_nodes=[64, 128], learning_rate=0.05, gamma=0.95, ER_size=0, update_TN=False):
         """
         :param n_actions: Number of actions available to agent
+        :param n_nodes: Number of nodes in hidden layers
         :param learning_rate: Learning rate of neural network
         :param gamma: discount factor, used to calculate the targets during the update
         :param ER_size: size of the experience replay buffer.
+        :param update_TN: boolean which sets the target network update on or off
         The experience replay buffer uses a Deque with an adjustable max size. When an item is added to a Deque
         with the max length, the first item is removed from the deque, and the new item appended at the end,
         keeping the size at the max length.
@@ -34,16 +36,21 @@ class DQN_agent:
             self.ER_buffer = deque(maxlen=ER_size)
 
     def select_action(self, s, strategy, epsilon=None, temp=None):
+		"""Select the action for a given state and strategy"""
+		
+		# Epsilon-greedy policy
         if strategy == "epsilon":
             if np.random.uniform() < epsilon:
                 return np.random.randint(low=0, high=self.n_actions)
             else:
                 return np.argmax(self.Q_model.predict(s)[0])
+                
+		# Boltzmann policy
         elif strategy == 'softmax':
             if temp is None:
                 raise KeyError("Provide a temperature")
 
-            x = self.Q_model.predict(s)
+            x = self.Q_model.predict(s)[0]
             y = softmax(x, temp)
             z = random.random()
             for i, val in enumerate(y):
@@ -56,11 +63,15 @@ class DQN_agent:
             return a
 
     def update(self, observations):
+		"""Update Q model"""
+		
         states = []
         targets = []
         for s, a, r, s_next, done in observations:
             states.append(s)
             Gt = r
+            
+            # Update the backup target with target or Q network
             if not done:
                 if self.update_TN:
                     Gt += self.gamma * np.amax(self.target_network.predict(s_next)[0])
@@ -71,22 +82,32 @@ class DQN_agent:
             targets.append(target)
         states = np.concatenate(states)
         targets = np.concatenate(targets)
+        
+        # Shuffle on False gives the episodes in chronological order
         self.Q_model.fit(states, targets, epochs=1, verbose=0, shuffle=False)
-        pass
 
     def make_q_model(self):
+		"""Construct the Q network with input, hidden and output layers"""
+		
+		# Make input layer
         inputs = layers.Input(shape=(4,))
         layers_list = [inputs]
-
+        
+        # Make hidden layers
         for i in range(len(self.n_nodes)):
             layers_list.append(layers.Dense(self.n_nodes[i], activation='sigmoid')(layers_list[i]))
-
+        
+        # Make output layer
         output = layers.Dense(self.n_actions, activation="linear")(layers_list[-1])
+        
+        # Combine input and output layers to construct the network
         model = keras.Model(inputs=inputs, outputs=output)
         model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
         return model
 
     def update_target_network(self):
+		"""Target network will be updated with current Q network"""
+		
         self.target_network.set_weights(self.Q_model.get_weights())
 
 
@@ -99,6 +120,8 @@ def q_learning(n_episodes=250,
     Return: rewards, a vector with the observed rewards at each timestep '''
 
     epsilon = epsilon_max
+    
+    # Initialize the right agent with(out) ER and/or TN
     if ER_buffer and update_TN:
         ER_batch = ER_batch
         agent = DQN_agent(gamma=gamma, learning_rate=learning_rate, ER_size=ER_size, update_TN=update_TN)
@@ -110,7 +133,7 @@ def q_learning(n_episodes=250,
     else:
         agent = DQN_agent(gamma=gamma, learning_rate=learning_rate)
     print(agent.Q_model)
-    # model = agent.make_q_model()
+    
     reward_per_episode = []
     env = gym.make("CartPole-v1")
     for i in range(n_episodes):
@@ -122,30 +145,45 @@ def q_learning(n_episodes=250,
         while not done:
             if render:
                 env.render()
+                
+            # Select action for given state and strategy
             action = agent.select_action(state, strategy, epsilon=epsilon, temp=temp)
+            
+            # Simulate environment
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, 4])
             episode.append((state, action, reward, next_state, done))
             rewards.append(reward)
             state = next_state
+        
         if update_TN:
+            # Update target network when at the right episode
             if i % n_update_TN == 0:
                 agent.update_target_network()
         reward_per_episode.append(np.sum(rewards))
+        
         print("episode: ", i, " score: ", reward_per_episode[-1], " Epsilon: ", epsilon)
+        
+        # Update Q network with or without ER buffer
         if ER_buffer:
             agent.ER_buffer.extend(episode)
             if len(agent.ER_buffer) > ER_batch:
                 agent.update(random.sample(agent.ER_buffer, ER_batch))
         else:
             agent.update(episode)
-        if epsilon > epsilon_min:
-            epsilon = epsilon * epsilon_decay
+        
+        # Decrease epsilon if exploration strategy is annealing epsilon-greedy    
+        if strategy="epsilon" and epsilon_decay != 1:
+		    if epsilon > epsilon_min:
+		        epsilon = epsilon * epsilon_decay
+            
     env.close()
     return reward_per_episode
 
 
 def test():
+	"""Test function which prints the obtained rewards for the parameters below"""
+	
     n_episodes = 500
     gamma = 1
     learning_rate = 0.05
@@ -165,9 +203,7 @@ def test():
     ER_size = 1000
     ER_batch = 50
 
-    # After how much episodes the target network will be updated
-    # When value of 1 is chosen > same as if no target network is used
-    # Because the target network will then be updated at every episode
+    # The update frequency of the target network
     update_TN = True
     n_update_TN = 25
 
